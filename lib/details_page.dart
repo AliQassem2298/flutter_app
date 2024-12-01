b4d1ad45e1bde9c1b5a588a1f21ac79aea53385f
AliQassem2298 <aliyeadqassem@gmail.com>
2024-12-01 15:05:30 +0300

    after the connecting with the back-end

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,34 +1,39 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/api.dart';
+import 'package:flutter_application_2/products_model.dart';
 
 class DetailsPage extends StatelessWidget {
   DetailsPage({
     super.key,
-    required this.productName,
-    required this.image,
-        required this.productDescription,
-    required this.productNumber,
-
+    // required this.productName,
+    // required this.image,
+    //     required this.productDescription,
+    // required this.productNumber,
+    required this.product,
   });
+  ProductsModel product;
 
-  String image, productName;
-  String productDescription;
-  int productNumber;
+  // String image, productName;
+  // String productDescription;
+  // int productNumber;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Details Page"),
+        title: const Text("Details Page"),
       ),
       body: Column(
         children: [
-          Image.asset(
-            image,
+          Image.network(
+            '$baseUrlImage${product.image}',
             width: 300,
             height: 270,
           ),
-          Text(productName),
-          Text(productDescription),
-          Text("Product Number is: $productNumber"),
+          Text(product.productName),
+          Text(product.description),
+          Text("Product Number is: ${product.number}"),
         ],
       ),
     );
--- lib/products_page.dart
+++ lib/products_page.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/api.dart';
 import 'package:flutter_application_2/details_page.dart';
+import 'package:flutter_application_2/products_model.dart';
+import 'package:flutter_application_2/products_service.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
@@ -7,46 +12,79 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Products"),
-      ),
-      body: ListView(
-        children: [
-          MyProduct(
-            productName: 'product 1',
-            image: 'assets/image1.jpg',
-            productDescription: 'Description for product 1',
-            productNumber: 1,
-          ),
-          MyProduct(
-            productName: 'product 2',
-            image: 'assets/image2.jpg',
-            productDescription: 'Description for product 2',
-            productNumber: 2,
-          ),
-          MyProduct(
-            productName: 'sara',
-            image: 'assets/image1.jpg',
-            productDescription: 'Description for product sara',
-            productNumber: 100,
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("My Products"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                ProductsService().products();
+              },
+              icon: const Icon(
+                Icons.abc,
+              ),
+            ),
+          ],
+        ),
+        body: FutureBuilder<List<ProductsModel>>(
+          future: ProductsService().products(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<ProductsModel> products = snapshot.data!;
+              return ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return MyProduct(
+                  product: products[index],
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        )
+        // ListView(
+        //   children: [
+        //     MyProduct(
+        //       productName: 'product 1',
+        //       image: 'assets/image1.jpg',
+        //       productDescription: 'Description for product 1',
+        //       productNumber: 1,
+        //     ),
+        //     MyProduct(
+        //       productName: 'product 2',
+        //       image: 'assets/image2.jpg',
+        //       productDescription: 'Description for product 2',
+        //       productNumber: 2,
+        //     ),
+        //     MyProduct(
+        //       productName: 'sara',
+        //       image: 'assets/image1.jpg',
+        //       productDescription: 'Description for product sara',
+        //       productNumber: 100,
+        //     ),
+        //   ],
+        // ),
+        );
   }
 }
 
 class MyProduct extends StatelessWidget {
   MyProduct({
     super.key,
-    required this.productName,
-    required this.image,
-    required this.productDescription,
-    required this.productNumber,
+    // required this.productName,
+    // required this.image,
+    // required this.productDescription,
+    // required this.productNumber,
+    required this.product,
   });
-  String image, productName;
-  String productDescription;
-  int productNumber;
+  // String image, productName;
+  // String productDescription;
+  // int productNumber;
+  ProductsModel product;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -56,10 +94,18 @@ class MyProduct extends StatelessWidget {
           MaterialPageRoute(
             builder: (context) {
               return DetailsPage(
-                productName: productName,
-                image: image,
-                productDescription: productDescription,
-                productNumber: productNumber,
+                // productName: productName,
+                // image: image,
+                // productDescription: productDescription,
+                // productNumber: productNumber,
+
+
+                // productName: product.productName,
+                // image: product.image,
+                // productDescription: product.description,
+                // productNumber: product.number,
+
+                product: product,
               );
             },
           ),
@@ -67,12 +113,12 @@ class MyProduct extends StatelessWidget {
       },
       child: Row(
         children: [
-          Image.asset(
-            image,
+          Image.network(
+            '$baseUrlImage${product.image}',
             width: 100,
             height: 90,
           ),
-          Text(productName),
+          Text(product.productName),
         ],
       ),
     );
